9d14d2040fd82979a6909b74757fdf80112c2d33
kspar <kaspar.papli@gmail.com>
2017-10-20 22:11:38 +0300

    Enable CORS for translation queries

--- src/main/java/ee/ut/sauron/controller/RestServicesController.java
+++ src/main/java/ee/ut/sauron/controller/RestServicesController.java
@@ -6,6 +6,7 @@ import ee.ut.sauron.service.TranslationService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class RestServicesController {
     private final TranslationService translationService;
 
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, path = "/v1.0/translate")
+    @CrossOrigin
     public ResponseDTO translationQuery(RequestDTO requestDTO) {
 
         log.info(requestDTO.toString());
